93b6c04d0fe0949ad961d82ccc34d857b992f4b6
Timelaying <makanjuola.timi@gmail.com>
2025-06-23 18:53:56 +0100

    index.ts for all model imports

---
+++ shopping-taxi-app/src/app/backend/server_apis_db/models/index.ts
@@ -0,0 +1,11 @@
+export * as UserModel from './users.model';
+export * as StoreModel from './stores.model';
+export * as ItemModel from './items.model';
+export * as StoreItemModel from './storeItems.model';
+export * as TripModel from './trips.model';
+export * as TripItemModel from './tripItems.model';
+
+
+/*Now you can simply import all models like this:
+import { UserModel, TripModel } from '../models';
+*/

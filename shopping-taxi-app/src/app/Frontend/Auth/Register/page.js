587dcff4863736fe6b2d95fbf5c1e54d2b15edd5
Timelaying <makanjuola.timi@gmail.com>
2025-07-14 10:33:50 +0100

    fixing fetch

--- shopping-taxi-app/src/app/Frontend/Auth/Register/page.js
+++ shopping-taxi-app/src/app/Frontend/Auth/Register/page.js
@@ -24,7 +24,7 @@ export default function RegisterPage() {
     }
 
     try {
-      const res = await fetch("/api/auth/register", {
+      const res = await fetch("http://localhost:5000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, username, password }),

116ae8a5d2b68283757a784e45c0efe119e4ab50
Timelaying <makanjuola.timi@gmail.com>
2025-07-20 11:46:37 +0100

    fixing paths

--- shopping-taxi-app/src/app/Frontend/Customer/Auth/Register/page.tsx
+++ shopping-taxi-app/src/app/Frontend/Customer/Auth/Register/page.tsx
@@ -115,13 +115,13 @@ export default function RegisterPage() {
         </form>
                <p className="text-center text-sm text-gray-600 space-x-2">
           <span>Already have an account?</span>
-          <Link href="/Auth/Login" className="text-blue-600 hover:underline">
+          <Link href="/Frontend/Auth/Login" className="text-blue-600 hover:underline">
             Log in
           </Link>
         </p>
         <p className="text-center text-sm text-gray-600 space-x-2">
           <span>Are you a driver?</span>
-          <Link href="/Driver/Auth/Register" className="text-blue-600 hover:underline">
+          <Link href="/Frontend/Driver/Auth/Register" className="text-blue-600 hover:underline">
             Register as Driver
           </Link>
         </p>
